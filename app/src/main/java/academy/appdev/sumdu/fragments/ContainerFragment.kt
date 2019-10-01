6ccf8676e6512e2f486f28d95a2533e7283c3b13
Igor Gorovoy <igor@appdev.academy>
2019-10-01 12:25:36 +0300

    TabLayout content issue fix

--- app/src/main/java/academy/appdev/sumdu/fragments/ContainerFragment.kt
+++ app/src/main/java/academy/appdev/sumdu/fragments/ContainerFragment.kt
@@ -1,17 +1,11 @@
 package academy.appdev.sumdu.fragments
 
 import academy.appdev.sumdu.R
-import academy.appdev.sumdu.adapters.TabListAdapter
 import academy.appdev.sumdu.adapters.TabsAdapter
-import academy.appdev.sumdu.mainActivity
-import academy.appdev.sumdu.networking.getLists
-import academy.appdev.sumdu.objects.ListObject
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.container_fragment_layout.*
-import kotlinx.android.synthetic.main.tab_list_layout.*
 
 class ContainerFragment : Fragment() {
 
@@ -35,30 +29,23 @@ class ContainerFragment : Fragment() {
         setUpTabs()
     }
 
-//    override fun onResume() {
-//        super.onResume()
-//        tabsAdapter.notifyDataSetChanged()
-//    }
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_screen_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
     private fun setUpTabs() {
-        if (mainActivity?.supportFragmentManager != null) {
-            tabsAdapter = TabsAdapter(mainActivity!!.supportFragmentManager)
-            tabsAdapter.apply {
-                addFragment(HistoryTabFragment(), getString(R.string.history))
-                addFragment(GroupTabFragment(), getString(R.string.group))
-                addFragment(TeacherTabFragment(), getString(R.string.teacher))
-                addFragment(AuditoriumTabFragment(), getString(R.string.auditorium))
-            }
-
-            viewPager.adapter = tabsAdapter
-            tabLayout.setupWithViewPager(viewPager)
-
-            tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_history)
+        tabsAdapter = TabsAdapter(childFragmentManager)
+        tabsAdapter.apply {
+            addFragment(HistoryTabFragment(), getString(R.string.history))
+            addFragment(GroupTabFragment(), getString(R.string.group))
+            addFragment(TeacherTabFragment(), getString(R.string.teacher))
+            addFragment(AuditoriumTabFragment(), getString(R.string.auditorium))
         }
+
+        viewPager.adapter = tabsAdapter
+        tabLayout.setupWithViewPager(viewPager)
+
+        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_history)
     }
 }
